732874c5fb9ca5be6c8ca3c9879dc678cac5f86a
Alex Stoyanov <loveca001@gmail.com>
2018-05-22 00:45:19 +0300

    removed resources

--- Java Advanced/Problems/LinearDataStructures - Exercises/src/InfixToPostfix.java
+++ Java Advanced/Problems/LinearDataStructures - Exercises/src/InfixToPostfix.java
@@ -1,115 +1,71 @@
-import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 public class InfixToPostfix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Deque<String> input = new ArrayDeque<>();
+        String[] infixArgs = scanner.nextLine().split(" ");
 
-        Collections.addAll(input, scanner.nextLine().split("\\s+"));
+        System.out.println(convertToPostFix(infixArgs));
+    }
+
+    private static String convertToPostFix(String[] infixArgs) {
+        Deque<String> tokenStack = new ArrayDeque<>();
 
         Deque<String> outputQueue = new ArrayDeque<>();
-        Deque<String> operatorStack = new ArrayDeque<>();
 
-        while (input.size() != 0) {
-            String token = input.poll();
-
-            if (isInteger(token)) {
-                outputQueue.offer(token);
-            } else if (isLeftBracket(token)) {
-                operatorStack.push(token);
-            } else if (isRightBracket(token)) {
-                String operator = operatorStack.pop();
-
-                while (true) {
-                    if (operator.equals("(")) {
-                        if (operatorStack.size() != 0) {
-                            outputQueue.offer(operatorStack.pop());
-                        }
-                        break;
-                    }
-
-                    outputQueue.add(operator);
-                    operator = operatorStack.pop();
+        for (String operand : infixArgs) {
+            if (isOperand(operand)) {
+                outputQueue.offer(operand);
+            } else if (operand.equals("(")) {
+                tokenStack.push(operand);
+            } else if (operand.equals(")")) {
+                while (!tokenStack.isEmpty() && !tokenStack.peek().equals("(")) {
+                    outputQueue.offer(tokenStack.pop());
                 }
-            } else if (isOperator(token)) {
-                String topOfStack = null;
 
-                if (operatorStack.size() != 0) {
-                    topOfStack = operatorStack.peek();
+                if (tokenStack.size() != 0) {
+                    tokenStack.pop();
                 }
-                operatorStack.push(token);
-
-                while (true) {
-                    if (operatorStack.size() > 1) {
-                        if (hasHigherPrecedence(topOfStack, token) && isOperator(topOfStack)) {
-                            topOfStack = operatorStack.pop();
-                            outputQueue.offer(topOfStack);
-                        } else {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-
-                    if (operatorStack.size() != 0) {
-                        topOfStack = operatorStack.peek();
-                    }
-
-                    if (operatorStack.size() == 0) {
-                        break;
-                    }
+            } else {
+                if (!tokenStack.isEmpty() && getPrecedence(operand) <= getPrecedence(tokenStack.peek())) {
+                    outputQueue.offer(tokenStack.pop());
                 }
-            } else if (isVariable(token)) {
-                outputQueue.offer(token);
+                tokenStack.push(operand);
             }
         }
 
-        while (operatorStack.size() != 0) {
-            outputQueue.offer(operatorStack.pop());
+        while (!tokenStack.isEmpty()) {
+            outputQueue.offer(tokenStack.pop());
         }
 
-        System.out.println(String.join(" ", outputQueue));
+        return String.join(" ", outputQueue);
     }
 
-    private static boolean isVariable(String token) {
-        return token.charAt(0) >= 97 && token.charAt(0) <= 122;
+    private static int getPrecedence(String operand) {
+        switch (operand) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+        }
+
+        return -1;
     }
 
-    private static boolean hasHigherPrecedence(String topOfStack, String token) {
-        if (topOfStack.equals("/") || topOfStack.equals("*") && token.equals("/") || token.equals("*")) {
-            return true;
-        } else if ((topOfStack.equals("+") || topOfStack.equals("-")) && (token.equals("+") || token.equals("-"))) {
-            return true;
-        } else if (topOfStack.equals("+") || topOfStack.equals("-") && (token.equals("/") || token.equals("*"))) {
-            return false;
-        } else if (topOfStack.equals("*") || topOfStack.equals("/") && (token.equals("+") || token.equals("-"))) {
+//    private static boolean isOperator(String operand) {
+//        return operand.equals("+") || operand.equals("-") || operand.equals("/") || operand.equals("*");
+//    }
+
+    private static boolean isOperand(String operand) {
+        if (operand.charAt(0) >= 97 && operand.charAt(0) <= 122) {
             return true;
         }
 
-        return false;
-    }
-
-    private static boolean isOperator(String token) {
-        return token.equals("+") || token.equals("*") || token.equals("/") || token.equals("-");
-    }
-
-    private static boolean isLeftBracket(String token) {
-        return token.equals("(");
-
-    }
-
-    private static boolean isRightBracket(String token) {
-        return token.equals(")");
-
-    }
-
-    private static boolean isInteger(String token) {
         try {
-            int number = Integer.parseInt(token);
+            int num = Integer.parseInt(operand);
             return true;
         } catch (NumberFormatException ex) {
             return false;
