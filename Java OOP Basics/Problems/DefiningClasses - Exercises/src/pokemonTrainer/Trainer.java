cdc1034654d062ab3d22438cf1811599ab558a39
Alex Stoyanov <loveca001@gmail.com>
2018-06-14 03:03:23 +0300

    more problems

--- Java OOP Basics/Problems/DefiningClasses - Exercises/src/pokemonTrainer/Trainer.java
+++ Java OOP Basics/Problems/DefiningClasses - Exercises/src/pokemonTrainer/Trainer.java
@@ -1,4 +1,11 @@
 package pokemonTrainer;
 
+import java.util.List;
+
 public class Trainer {
+    private String name;
+    private int badges;
+    private List<Pokemon> pokemons;
+
+    public Trainer(String name);
 }
